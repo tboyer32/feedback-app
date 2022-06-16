97513b35e21312137e960673db13f4902e4f29cb
Tiffany Boyer <tboyercode@gmail.com>
2022-06-16 15:15:37 -0500

    add Card component

---
+++ src/components/shared/Card.js
@@ -0,0 +1,11 @@
+function Card({ children, reverse }) {
+  return (
+    <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+  )
+}
+
+Card.defaultProps = {
+    reverse: false
+}
+
+export default Card
