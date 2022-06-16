52323bb5713bcbb92df7b8f2c16b30053adc813a
Tiffany Boyer <tboyercode@gmail.com>
2022-06-15 21:15:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const title = "Blog Post";
+    const body = "This is my blog post";
+    const list = [
+        {id: 1, text: 'comment 1'},
+        {id: 2, text: 'comment 2'},
+        {id: 3, text: 'comment 3'}
+    ];
+
+    return (
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{body}</p>
+        </div>
+    );
 }
 
 export default App;
